3ec105026fc1c1bbc8b52cf0538bb50c4f341b7b
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2022-06-29 15:03:35 -0600

    JS Advanced, 04 - DOM Introduction, Exercise 03.

--- 04-DOM Introduction/03. Accordion/accordion.js
+++ 04-DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonText = document.getElementsByClassName('button')[0].textContent;
+
+    if (buttonText === 'More'){
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+
+        document.getElementById('extra').style.display = 'block';
+    } else {
+        document.getElementsByClassName('button')[0].textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
 }
