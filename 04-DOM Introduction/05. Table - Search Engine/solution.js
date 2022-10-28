9693574fbb4d846be31cd63a18547ef60d728379
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2022-10-28 11:46:11 -0400

    JS Advanced, 04 - DOM Introduction, Exercise 05 and 06.

--- 04-DOM Introduction/05. Table - Search Engine/solution.js
+++ 04-DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,23 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let searchField = document.getElementById('searchField').value;
+
+      let tableRows = document.querySelectorAll('tbody tr');
+
+      for (const tableRow of tableRows) {
+         tableRow.classList.remove('select');
+      }
+
+      for (const tableRow of tableRows) {
+         let tableCols = tableRow.childNodes;
+
+         for (const tableCol of tableCols) {
+            if (tableCol.textContent.includes(searchField)){
+               tableCol.parentElement.classList.add("select");
+            }
+         }
+      }
 
    }
 }
--- 04-DOM Introduction/06. Format the Text/solution.js
+++ 04-DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value;
+
+  let textArray = text.split('.').filter(n => n);
+
+  // console.log(textTokens);
+
+  let outputElement = document.getElementById('output');
+
+  // for (const textToken of textTokens) {
+  //   const p = document.createElement('p');
+  //   p.textContent = textToken;
+  //   outputElement.appendChild(p);
+  // }
+
+  while (textArray.length > 0){
+    const paragraphToAdd = textArray.splice(0,3);
+
+    const p = document.createElement('p');
+    p.textContent = paragraphToAdd.join('. ') + '.';
+
+    outputElement.appendChild(p);
+  }
 }
